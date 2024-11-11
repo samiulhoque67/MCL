6f2a43eb3dd2fcd54e75028e8acd4d8dabc6c7bd
NaimulNaim <naimul.hasan.naim@g.bracu.ac.bd>
2024-11-11 15:18:28 +0600

    commit

--- SILDMS.DataAccess/QuotationApproval/IQuotationApprovalDataService.cs
+++ SILDMS.DataAccess/QuotationApproval/IQuotationApprovalDataService.cs
@@ -11,6 +11,9 @@ namespace SILDMS.DataAccess.QuotationApproval
     {
         List<OBS_ClientwithReqQoutn> AllAvailableCSVendorApprovalDataService(string UserId, int page, int itemsPerPage, string sortBy, bool reverse, string search, string type, out string _errorNumber);
         List<ClientReqData> GetClientReqDataInfoDataService(string ClientID, out string _errorNumber);
+        List<OBS_TermsItem> GetVendorTermListServiceData(string ClientQutnRecmID, out string _errorNumber);
+
+
         string SaveQuotToClientServiceData(string UserID, List<OBS_QutntoClientMaster> MasterData, List<ClientReqData> DetailData, List<OBS_TermsItem> AllTermsDtl, out string _errorNumber);
 
     }
