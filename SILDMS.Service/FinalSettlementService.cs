3bc1eabba0b31790a0b4afac232a5407ccb6f4ae
Amir Hamza <hamza@squaregroup.com>
2025-03-10 14:33:02 +0600

    commit

--- SILDMS.Service/FinalSettlementService.cs
+++ SILDMS.Service/FinalSettlementService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SILDMS.DataAccessInterface;
+using SILDMS.Model;
+using SILDMS.Utillity.Localization;
+using SILDMS.Utillity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,31 @@ namespace SILDMS.Service
 {
     public class FinalSettlementService: IFinalSettlementService
     {
+        private readonly IFinalSettlementDataService _FinalSettlementData;
+        private readonly ILocalizationService _localizationService;
+        private string errorNumber = "";
+
+        public FinalSettlementService(IFinalSettlementDataService FinalSettlementData, ILocalizationService localizationService)
+        {
+            _FinalSettlementData = FinalSettlementData;
+            _localizationService = localizationService;
+        }
+
+        public ValidationResult GetfinalSettlementSearchList(out List<OBS_FinalSettlement> poSearchList)
+        {
+            poSearchList = _FinalSettlementData.GetfinalSettlementSearchList();
+            return ValidationResult.Success;
+        }
+
+        public ValidationResult GetShowfinalSettlementList(out List<OBS_FinalSettlement> vendorReqList)
+        {
+            vendorReqList = _FinalSettlementData.GetShowfinalSettlementList();
+            return ValidationResult.Success;
+        }
+
+        public string SavefinalSettlement(OBS_FinalSettlement billRecv)
+        {
+            return _FinalSettlementData.SavefinalSettlement(billRecv);
+        }
     }
 }
